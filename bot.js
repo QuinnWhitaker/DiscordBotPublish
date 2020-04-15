65cb68dd094f84da3a8b8b9e071c29de71235f6c
QuinnWhitaker <qmwhitaker@gmail.com>
2020-04-14 19:00:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,15 @@ client.on('message', message => {
 	
 	if (message.content.startsWith(voteCommand)) {
 		
+		var fs = require('fs');
+
+		fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+			
+			if (err) throw err;
+		  
+			console.log('Saved!');
+		  
+		});
 		
 	}
 });
