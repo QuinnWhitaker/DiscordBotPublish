119d917a49a34b584ce874599a05c4aea71ac372
QuinnWhitaker <qmwhitaker@gmail.com>
2020-04-14 18:55:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@
 // Import the discord.js module
 const Discord = require('discord.js');
 
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
@@ -21,12 +24,6 @@ client.on('message', message => {
 	
 	if (message.content.startsWith(voteCommand)) {
 		
-		var fs = require('fs');
-
-		fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-		  if (err) throw err;
-		  console.log('Saved!');
-		});
 		
 	}
 });
