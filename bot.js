a3be5f15151b3caa7d7278f33b387b76774e868d
QuinnWhitaker <qmwhitaker@gmail.com>
2020-04-14 19:45:33 -0700

    Update bot.js and pseudoscript.txt

--- bot.js
+++ bot.js
@@ -6,33 +6,66 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
- 
+// The id of the channel where active votes will be posted.
+// const voteChannelId = '677690362029932594'; // Main channel - comment out when testing
+const voteChannelId = '685413820096577573'; // Test channel - comment out when deploying
+
+// Declare the prefix command for voting
+const voteCommand = '!vote1 ';
+
+// What does it say next to someone's name when they haven't voted?
+const noVote = 'Vote Pending';
+
+
+// When the bot is ready 
 client.on('ready', () => {
 	
   console.log('I am ready!');
   
 });
 
-// Create an event listener for messages
+function formatPollString(vote_title, issued_by, vote_status, vote_dictionary, ) {
+	
+	var poll = 	'==== **VOTE: ' + vote_title 	+ '** ==== \n\n **Issued By:** ' 	+ issued_by + '\n\n' 
+	poll += 	'\n**Status: ' 	+ vote_status 	+ '\n\n'
+
+	return poll
+}
+
+// Whenever a user types a message
 client.on('message', message => {
 	
-	var voteCommand = '!vote1 ';
-	
+	// If the message starts with the prefix command
 	if (message.content.startsWith(voteCommand)) {
 		
+		// Get all content of the message beyond the prefix.
+		const postPreFix = message.content.replace(voteCommand, '');
+		
+		// Get the title of the vote (Same as postPreFix until multiple options have been implemented)
+		const voteTitle = postPreFix;
+		
+		// Get the author of the message
+		const issuedBy = message.author.toString();
+		
+		// Declare the status of the vote as ACTIVE
+		var voteStatus = 'ACTIVE';
+		
+		var voteDictionary = 1;
+		
+		console.log( formatPollString(voteTitle, issuedBy, voteStatus, voteDictionary) );
+	
+		// Create a local file documenting the poll and its contents
+		/*
 		var fs = require('fs');
 
-		fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+		fs.appendFile('.\votes\mynewfile1.txt', 'Hello content!', function (err) {
 			
 			if (err) throw err;
 		  
-			console.log('Saved!');
+			console.log('Saved file!');
 		  
 		});
+		*/
 		
 	}
 });
